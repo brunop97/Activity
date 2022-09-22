5f6f5f4732a1ad0bcf078af0179ca41da69e40e1
brunop97 <bruno.peroni97@gmail.com>
2022-09-22 09:30:13 -0300

    Revisado Acitivities

---
+++ calculamedia/app/src/main/java/com/example/calculamedia/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.calculamedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public  void botaoCalcularOnClick (View v){
+        EditText np1Digitado = (EditText) findViewById(R.id.editTextNumberDecimal);
+        String np1String = np1Digitado.getText().toString();
+        double np1 = Double.parseDouble(np1String);
+
+        EditText np2Digitado = (EditText) findViewById(R.id.editTextNumberDecimal);
+        String np2String = np2Digitado.getText().toString();
+        double np2 = Double.parseDouble(np1String);
+
+        double media = (np1 + np2) / 2;
+
+        Intent telaResultado = new Intent(this, Resultado.class);
+        telaResultado.putExtra("media", media);
+
+        startActivity(telaResultado);
+
+    }
+}
